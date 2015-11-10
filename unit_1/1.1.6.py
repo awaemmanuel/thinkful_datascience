6d5d03bb03e6f83f019f72848eb452124c921aed
Emmanuel Awa <eawa@brandeis.edu>
2015-11-09 23:15:03 -0500

    Adding first solution

---
+++ unit_1/1.1.6.py
@@ -0,0 +1,37 @@
+import collections, math
+
+populations_2010 = collections.defaultdict(int)
+populations_2100 = collections.defaultdict(int)
+populations_diff = collections.defaultdict(int)
+
+
+with open('lecz-urban-rural-population-land-area-estimates-v2-csv/lecz-urban-rural-population-land-area-estimates_continent-90m.csv', 'rU') as input_file:
+    header = next(input_file).split(',')
+    
+    for line in input_file:
+        line = line.rstrip().split(',')
+        if line[1] == 'Total National Population':
+            populations_2010[line[0]] += int(line[5])
+            populations_2100[line[0]] += int(line[6])
+
+            
+with open('national_population.csv', 'w') as output_file:
+    output_file.write('continent,2010_population\n')
+    
+    for k, v in populations_2010.iteritems():
+       output_file.write(k + ',' + str(v) + '\n')
+      
+# Q1 Calculate the rate of change
+for continent in populations_2010.keys():
+    continent_2100 = populations_2100.get(continent)
+    continent_2010 = populations_2010.get(continent)
+    populations_diff[continent] = (((continent_2100 - continent_2010)) / float(continent_2010)) * 100
+
+
+print "The continent estimated to grow the most in the next 90 years is:  {}".format(max(populations_diff.iterkeys(), key = (lambda key : populations_diff[key])))
+
+
+print "=" * 50
+
+
+# Q2. 
